28f05dfa7d8d0d1f733758a49a1a4aa54893f718
unknown <rumelchow340@gmail.com>
2025-02-06 11:24:14 +0600

    I have create not found route and custom error handler middlware

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./connectDB/connectDb");
+const { notFoundRouter, customErrorHandler } = require("./middleware/errorHandler/errorHnadler");
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-//middlware
 //parse json data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// connect to Mongodb database
 connectDB();
+
+// wrong route
+app.use(notFoundRouter);
+app.use(customErrorHandler);
+
 // listen to the server port on 3000
 app.listen(port, () => {
   console.log("app running on port", port);
--- middleware/errorHandler/errorHnadler.js
+++ middleware/errorHandler/errorHnadler.js
@@ -1,3 +1,13 @@
-const notFoundRouter = function (req, res, next) {
-  res.send("this page not fouind");
+const notFoundRouter = (req, res, next) => {
+  const errors = new Error("Not found anything");
+  next(errors);
 };
+const customErrorHandler = (err, req, res, next) => {
+  // console.log(err);
+  res.status(err.status || 500).json({
+    error: err.status === 404 ? "Not Found" : "Internal Server Error",
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
+};
+module.exports = { notFoundRouter, customErrorHandler };
